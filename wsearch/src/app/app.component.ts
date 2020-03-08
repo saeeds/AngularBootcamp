171bfc132a5e92f994527d7f639fef9f393de341
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2020-03-08 18:53:07 +0300

    add wikipedia search project

---
+++ wsearch/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+import { WikipediaService } from "./wikipedia.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  pages = [];
+
+  constructor(private wikipediaService: WikipediaService) {}
+
+  onTerm(term: string) {
+    this.wikipediaService.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
+    });
+  }
+}
